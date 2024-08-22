6e1ab067dfe932c9ba1b9d23e91ef65cd543f179
manarjw <manomu752@gmail.com>
2024-08-22 22:07:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// تبديل حالة القائمة المنسدلة
+        function toggleMenu() {  
+            const menu = document.querySelector('.list');  
+            const menuBtn = document.querySelector('.menu-btn');  
+            menu.classList.toggle('active'); // تبديل إظهار وإخفاء القائمة  
+            menuBtn.classList.toggle('active'); // تبديل حالة زر القائمة  
+}
+
